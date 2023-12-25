aeb784f6c844344e78bf843bdeea43505bbb807a
moktubat <shadhinreja@gmail.com>
2023-12-26 03:27:23 +0600

    navbar added

--- src/pages/Home/Shared/NavBar/NavBar.jsx
+++ src/pages/Home/Shared/NavBar/NavBar.jsx
@@ -1,10 +1,135 @@
+import { Link, NavLink } from "react-router-dom";
+import { HiOutlineBars3BottomRight } from "react-icons/hi2";
+import { FaXmark } from "react-icons/fa6";
+import { useState } from "react";
 
 const NavBar = () => {
-    return (
-        <div>
-            <p>I am NavBar</p>
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const navLinks = [
+    <>
+      <li>
+        <NavLink
+          to="/"
+          className={({ isActive }) => (isActive ? "active" : "default")}
+        >
+          Home
+        </NavLink>
+      </li>
+      <li>
+        <NavLink
+          to="/about"
+          className={({ isActive }) => (isActive ? "active" : "default")}
+        >
+          About
+        </NavLink>
+      </li>
+      <li>
+        <NavLink
+          to="/workouts"
+          className={({ isActive }) => (isActive ? "active" : "default")}
+        >
+          Workout Programs
+        </NavLink>
+      </li>
+      <li>
+        <NavLink
+          to="/recipes"
+          className={({ isActive }) => (isActive ? "active" : "default")}
+        >
+          Recipes
+        </NavLink>
+      </li>
+      <li>
+        <NavLink
+          to="/store"
+          className={({ isActive }) => (isActive ? "active" : "default")}
+        >
+          Store
+        </NavLink>
+      </li>
+      <li>
+        <NavLink
+          to="/blogs"
+          className={({ isActive }) => (isActive ? "active" : "default")}
+        >
+          Blogs
+        </NavLink>
+      </li>
+      <li>
+        <NavLink
+          to="/contact"
+          className={({ isActive }) => (isActive ? "active" : "default")}
+        >
+          Contact
+        </NavLink>
+      </li>
+      <li>
+        <NavLink
+          to="/login"
+          className={({ isActive }) => (isActive ? "active" : "default")}
+        >
+          Login
+        </NavLink>
+      </li>
+    </>,
+  ];
+  return (
+    <div className="px-4 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
+      <div className="relative flex items-center justify-between">
+        {/* Logo Section */}
+        <Link to="/" className="inline-flex items-center">
+          <span className="ml-2 text-xl font-bold tracking-wide text-[#FFED00]">
+            absMania
+          </span>
+        </Link>
+
+        {/* Nav Items Section */}
+        <ul className="items-center hidden space-x-8 lg:flex">{navLinks}</ul>
+        {/* Mobile Navbar Section */}
+        <div className="lg:hidden">
+          {/* Dropdown Open Button */}
+          <button
+            aria-label="Open Menu"
+            title="Open Menu"
+            onClick={() => setIsMenuOpen(true)}
+          >
+            <HiOutlineBars3BottomRight className="w-8 text-white" />
+          </button>
+          {isMenuOpen && (
+            <div className="absolute top-0 left-0 w-full z-10">
+              <div className="p-5 bg-black border rounded shadow-sm">
+                {/* Logo & Button section */}
+                <div className="flex items-center justify-between mb-4">
+                  <div>
+                    <Link to="/" className="inline-flex items-center">
+                      <span className="ml-2 text-xl font-bold tracking-wide text-[#FFED00] uppercase">
+                        absMania
+                      </span>
+                    </Link>
+                  </div>
+                  {/* Dropdown menu close button */}
+                  <div>
+                    <button
+                      aria-label="Close Menu"
+                      title="Close Menu"
+                      onClick={() => setIsMenuOpen(false)}
+                    >
+                      <FaXmark className="w-5 text-[#FFED00]" />
+                    </button>
+                  </div>
+                </div>
+                {/* Mobile Nav Items Section */}
+                <nav>
+                  <ul className="space-y-4">{navLinks}</ul>
+                </nav>
+              </div>
+            </div>
+          )}
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default NavBar;
